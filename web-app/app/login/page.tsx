d60c0fba16009fb011624ad985c878886bae927d
kavindu-udara <karunasinghesampath38@gmail.com>
2025-11-02 07:05:40 +0530

    created a nextapp and init a login page for testing

---
+++ web-app/app/login/page.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const LoginPage = () => {
+  return (
+    <main className='flex justify-center items-center min-h-screen'>
+      <div className='grid grid-cols-4 h-24'>
+        <div className='border rounded-xl p-5 flex items-center justify-center'>
+            hola
+        </div>
+      </div>
+    </main>
+  )
+}
+
+export default LoginPage
